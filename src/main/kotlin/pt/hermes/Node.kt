6b6e3adfdf84b4abf93ac29f411d661d17435290
rodri <rodrigotteixeira7@gmail.com>
2025-11-08 01:27:58 +0000

    feat: initialize network service

--- src/main/kotlin/pt/hermes/Node.kt
+++ src/main/kotlin/pt/hermes/Node.kt
@@ -6,16 +6,26 @@ import pt.hermes.blockchain.BlockchainService
 import pt.hermes.routing.configureRouting
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.plugins.contentnegotiation.*
+import pt.hermes.network.NetworkService
 
 fun main(args: Array<String>) {
     EngineMain.main(args)
 }
 
 fun Application.module() {
-    install(ContentNegotiation) {
-        json() // uses kotlinx.serialization
-    }
+    // Json serialization
+    install(ContentNegotiation) { json() }
 
+    // Network configuration
+    val networkConfig = environment.config.config("ktor.network")
+    val peers = networkConfig.property("peers").getList()
+
+    // Initialize services
     val blockchain = BlockchainService()
-    configureRouting(blockchain)
+    val network = NetworkService(peers, blockchain)
+
+    configureRouting(
+        blockchain,
+        network
+    )
 }
