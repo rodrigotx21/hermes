d20ac018520cec6f01fe80acb71528f025b326ba
rodri <rodrigotteixeira7@gmail.com>
2025-11-12 15:50:05 +0000

    feat: update routing configuration to include StorageService for state management

--- src/main/kotlin/pt/hermes/routing/Routing.kt
+++ src/main/kotlin/pt/hermes/routing/Routing.kt
@@ -4,13 +4,17 @@ import io.ktor.server.application.*
 import pt.hermes.blockchain.BlockchainService
 import pt.hermes.consensus.ConsensusService
 import pt.hermes.network.NetworkService
+import pt.hermes.storage.StorageService
 
 fun Application.configureRouting(
     blockchain: BlockchainService,
     network: NetworkService,
-    consensus: ConsensusService
+    consensus: ConsensusService,
+    storage: StorageService
 ) {
     blockchainRouting(blockchain)
     transactionsRouting(blockchain)
     networkRouting(blockchain, network)
+    stateRouting(storage)
+    walletRouting(blockchain)
 }
