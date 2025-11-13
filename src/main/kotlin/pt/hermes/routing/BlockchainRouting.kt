88e7a2d3aeb708bc43f7027875aa27bca688e3be
rodri <rodrigotteixeira7@gmail.com>
2025-11-13 23:58:19 +0000

    feat: add endpoints for retrieving blocks by index and current chain tip

--- src/main/kotlin/pt/hermes/routing/BlockchainRouting.kt
+++ src/main/kotlin/pt/hermes/routing/BlockchainRouting.kt
@@ -5,6 +5,8 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import pt.hermes.blockchain.BlockchainService
+import pt.hermes.network.Message.TipResponse
+
 fun Application.blockchainRouting(
     blockchain: BlockchainService
 ) {
@@ -30,6 +32,23 @@ fun Application.blockchainRouting(
 
                 val block = blockchain.chain[index]
                 call.respond(HttpStatusCode.OK, block)
+            }
+
+            get("/from/{index}") {
+                val indexParam = call.parameters["index"]
+                val index = indexParam?.toIntOrNull()
+                if (index == null || index < 0 || index >= blockchain.chain.size) {
+                    call.respond(HttpStatusCode.BadRequest, "Invalid block index")
+                    return@get
+                }
+
+                val blocks = blockchain.chain.subList(index, blockchain.chain.size)
+                call.respond(HttpStatusCode.OK, blocks)
+            }
+
+            get("/tip") {
+                val response = blockchain.getTip()
+                call.respond(HttpStatusCode.OK, response)
             }
         }
     }
